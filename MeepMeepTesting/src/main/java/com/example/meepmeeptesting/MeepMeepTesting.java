f4f7452fb54c2bc6f72907ac6fa17fc4d68b79ba
Darius <ddariusd007@gmail.com>
2024-07-12 00:18:09 -0400

    changed to red far

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -15,16 +15,35 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15.5)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new  Pose2d(11.3, -59.7, Math.toRadians(90)))
-                                .splineToSplineHeading(new Pose2d(21.84, -18, Math.toRadians(180.00)), Math.toRadians(90.00))
+                        drive.trajectorySequenceBuilder(new Pose2d(-34.71, -62.5, Math.toRadians(90)))
+                                .forward(3)
+                                .strafeLeft(3)
+                                .splineToSplineHeading(new Pose2d(-33.78, -34.5, Math.toRadians(0.00)), Math.toRadians(-17.11),
+                                        SampleMecanumDrive.getVelocityConstraint(30, 30, 9.335),
+                                        SampleMecanumDrive.getAccelerationConstraint(40))
+                                .forward(4)
+                                .back(5)
 
-                                .forward(7)
+                                .back(3)
+                                .waitSeconds(.4)
+
+                                .forward(1)
+                                .waitSeconds(.2)
+                                .back(6)
+
+                                .lineToConstantHeading(new Vector2d(-35.75, -10.20))
+                                .lineToSplineHeading(new Pose2d(27.69, -10.60, Math.toRadians(180.00)))
+                                //.turn(Math.toRadians(-45))
+                                .lineTo(new Vector2d(23.6, -28.4))
+
                                 .waitSeconds(1)
+                                .lineToConstantHeading(new Vector2d(49, -40),
+                                        SampleMecanumDrive.getVelocityConstraint(30, 30, 9.335),
+                                        SampleMecanumDrive.getAccelerationConstraint(40))
 
-
-                                .lineToSplineHeading(new Pose2d(45, -36.25, Math.toRadians(-180)))
-
-
+                                .back(5,
+                                        SampleMecanumDrive.getVelocityConstraint(30, 30, 9.335),
+                                        SampleMecanumDrive.getAccelerationConstraint(40))
                                 .build()
 
 

3c6949c6e9ce9257101a0fbbe8fdd0ff9043bf41
WIng57 <ddariusd007@gmail.com>
2024-07-16 19:33:28 -0400

    support for kotlin + constants file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Constants.kt
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.subsystems
+
+object Constants {
+
+    object ServoConstants {
+        const val RIGHT_ARM = "rA"
+        const val LEFT_ARM = "lA"
+        const val PLANE = "dr"
+        const val INTAKE = "in"
+        const val RELEASE = "release"
+
+        const val SCORE_PIVOT = 0.47
+        const val RELEASE_OPEN = 0.8
+        const val RELEASE_CLOSE = 0
+    }
+}
